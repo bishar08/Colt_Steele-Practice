892b0446285c34134408614836e4ec145291eff5
bishar08 <abubushayra@gmail.com>
2021-01-27 17:09:30 -0600

    comments of js

--- JavaScript/DOM/DOM_Intro_CODE/Manipulating/app.js
+++ JavaScript/DOM/DOM_Intro_CODE/Manipulating/app.js
@@ -1,18 +1,19 @@
-// const allLinks = document.querySelectorAll('a');
-
-// // for (let link of allLinks) {
-// //     link.innerText = 'I AM A LINK!!!!'
-// // }
-
+const allLinks = document.querySelectorAll('a');
 
 // for (let link of allLinks) {
-//     link.style.color = 'rgb(0, 108, 134)';
-//     link.style.textDecorationColor = 'magenta';
-//     link.style.textDecorationStyle = 'wavy'
+//     link.innerText = 'I AM A LINK!!!!'
 // }
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
 
 const allImages = document.getElementsByTagName('img')
 
 for (let img of allImages){
     console.log(img.src)
 }
+
